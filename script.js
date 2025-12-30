4c64940270f5395a0dd2b8444384201196ed32a3
alirezaxri91-png <alirezaxri91@gmail.com>
2025-12-30 20:30:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function send() {
-  let question = document.getElementById("input").value
+  let question = document.getElementById("input").value.toLowerCase()
 
-  let result =
-`# کد پایتون تولید شده
-# سوال
-# ${question}
+  let result = ""
 
-import math
+  if (question.includes("معادله درجه دو")) {
+    result = `# حل معادله درجه دو
+a = 1
+b = -3
+c = 2
 
-print("در حال حل مسئله ریاضی")`
+delta = b**2 - 4*a*c
+x1 = (-b + delta**0.5) / (2*a)
+x2 = (-b - delta**0.5) / (2*a)
+
+print(x1, x2)`
+  } else {
+    result = "# هنوز این سوال پشتیبانی نمیشه"
+  }
 
   document.getElementById("output").innerText = result
 }
